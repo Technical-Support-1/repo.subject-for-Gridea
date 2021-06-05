a09394c08945f354f8d8575af2eb2d4b95906a53
Technical-Support-1 <80965507+Technical-Support-1@users.noreply.github.com>
2021-06-05 14:36:07 +0800

    Create 666.py

---
+++ 早期作品/666.py
@@ -0,0 +1,26 @@
+"""the first python game"""
+skip=3
+import random
+answer=random.randint(1,10)
+
+while skip>0:
+    temp=input("猜一猜数字")
+    guess=int(temp)
+
+    if guess==answer:
+        print("You are right!")
+        print("See you next time")
+        break
+    elif guess<answer:
+        print("You are wrong")
+        print("small")
+    elif guess>answer:
+        print("You are wrong")
+        print("big")
+    else:
+        print("Please enter the right NUMBER")
+    skip=skip-1
+       
+if guess!=answer:
+        print(answer)
+        print("Goodbye")

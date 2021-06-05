5fc47ef7d3f0f0038fff0708a67fe5bad899eb68
Technical-Support-1 <80965507+Technical-Support-1@users.noreply.github.com>
2021-06-05 14:33:18 +0800

    Update 444.py

--- 早期作品/444.py
+++ 早期作品/444.py
@@ -6,3 +6,11 @@ while p<=9:
         print(i,"*",p,"=",i*p)
         i=i+1
     p=p+1
+
+# or something others...
+
+a = input("Here's other way to do it(more simple),do you want to try?(Y/N)")
+if a == "Y":
+    for i in range(1,10):
+        for p in range(1,10):
+            print(i,"*",p,"=",i*p)
